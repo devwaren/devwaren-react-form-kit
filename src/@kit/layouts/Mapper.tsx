d247aba35fb8a64b43ff47e6c0d8ce89a50a2c2f
Waren Gador <waren.gador15@gmail.com>
2026-09-14 17:59:23 +0800

    update mapper

--- src/@kit/layouts/Mapper.tsx
+++ src/@kit/layouts/Mapper.tsx
@@ -3,24 +3,21 @@ import { Fragment, type Key, type ReactNode } from "react";
 type MapperProps<T> = {
   items: T[];
   children: (item: T, index: number) => ReactNode;
-  className?: string;
-  listFor: string;
 };
 
-function MapperInner<T>({
-  items,
-  children,
-  className,
-  listFor,
-}: MapperProps<T>) {
+function MapperInner<T>({ items, children }: MapperProps<T>) {
   return (
-    <ul className={className} aria-label={`list for ${listFor}`}>
+    <>
       {items.map((item, index) => {
         const { id } = item as { id?: Key };
 
-        return <Fragment key={id ?? index}>{children(item, index)}</Fragment>;
+        return (
+          <Fragment key={id ?? index}>
+            {children(item, index)}
+          </Fragment>
+        );
       })}
-    </ul>
+    </>
   );
 }
 
